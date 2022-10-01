9322fe890b984c33cf41c4a7220e885416e09efd
Void <57257404+into-the-v0id@users.noreply.github.com>
2022-10-01 18:03:09 +0200

    Added some ktor plugins

--- api/src/main/kotlin/org/chatRoom/api/plugin/HTTP.kt
+++ api/src/main/kotlin/org/chatRoom/api/plugin/HTTP.kt
@@ -1,10 +1,38 @@
 package org.chatRoom.api.plugin
 
 import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.cachingheaders.*
+import io.ktor.server.plugins.compression.*
+import io.ktor.server.plugins.conditionalheaders.*
+import io.ktor.server.plugins.defaultheaders.*
+import java.util.Base64
 
 fun Application.configureHTTP() {
+    install(DefaultHeaders) {
+        header("Server", "Ktor")
+    }
+    install(ConditionalHeaders) {
+        version { call, content ->
+            when (content) {
+                is OutgoingContent.ByteArrayContent -> listOf(
+                    EntityTagVersion(Base64.getEncoder().encodeToString(content.bytes()).hashCode().toString()),
+                )
+                else -> emptyList()
+            }
+        }
+    }
+    install(CachingHeaders) {
+        options { _, _ ->
+            CachingOptions(CacheControl.MaxAge(
+                maxAgeSeconds = 0,
+                mustRevalidate = true,
+                visibility = CacheControl.Visibility.Private,
+            ))
+        }
+    }
     install(CORS) {
         allowMethod(HttpMethod.Options)
         allowMethod(HttpMethod.Put)
@@ -13,4 +41,5 @@ fun Application.configureHTTP() {
 
         anyHost()
     }
+    install(Compression)
 }
--- api/src/main/kotlin/org/chatRoom/api/plugin/Routing.kt
+++ api/src/main/kotlin/org/chatRoom/api/plugin/Routing.kt
@@ -2,10 +2,12 @@ package org.chatRoom.api.plugin
 
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.autohead.*
 import org.chatRoom.api.route.userRouting
 import org.chatRoom.core.repository.UserRepository
 
 fun Application.configureRouting(userRepository: UserRepository) {
+    install(AutoHeadResponse)
     routing {
         route("/users") { userRouting(userRepository) }
     }
