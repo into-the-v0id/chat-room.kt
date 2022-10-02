76878bf75328c209cb1ea3b7dc9d5da744dc101a
Void <57257404+into-the-v0id@users.noreply.github.com>
2022-10-03 00:00:50 +0200

    Fixed handle validator

--- api/src/main/kotlin/org/chatRoom/api/validator/HandleValidator.kt
+++ api/src/main/kotlin/org/chatRoom/api/validator/HandleValidator.kt
@@ -3,5 +3,5 @@ package org.chatRoom.api.validator
 import org.apache.commons.validator.routines.RegexValidator
 
 object HandleValidator {
-    val instance = RegexValidator(arrayOf("^[a-zA-Z0-9-_]$"))
+    val instance = RegexValidator(arrayOf("^[a-zA-Z0-9\\-_]+$"))
 }
