4006425487e3b7a9ae4b8602c8874bca37290e12
Void <57257404+into-the-v0id@users.noreply.github.com>
2022-10-02 22:18:09 +0200

    Migrate on api server start again

--- api/src/main/kotlin/org/chatRoom/api/Application.kt
+++ api/src/main/kotlin/org/chatRoom/api/Application.kt
@@ -1,9 +1,12 @@
 package org.chatRoom.api
 
 import io.ktor.server.engine.*
+import org.chatRoom.core.db.MigrationManager
 
 fun main() {
-    val engine = ServiceContainer.koin.koin.get<ApplicationEngine>()
+    val migrationManager = ServiceContainer.koin.koin.get<MigrationManager>()
+    migrationManager.migrate()
 
+    val engine = ServiceContainer.koin.koin.get<ApplicationEngine>()
     engine.start(wait = true)
 }
