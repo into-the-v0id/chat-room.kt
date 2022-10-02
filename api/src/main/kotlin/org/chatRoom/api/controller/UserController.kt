08688f32979a41228a05cc95c7cb0e1b28d2dc32
Void <57257404+into-the-v0id@users.noreply.github.com>
2022-10-02 11:21:01 +0200

    Cleaned up user controller

--- api/src/main/kotlin/org/chatRoom/api/controller/UserController.kt
+++ api/src/main/kotlin/org/chatRoom/api/controller/UserController.kt
@@ -12,62 +12,69 @@ class UserController(private val userRepository: UserRepository) {
     fun register(application: Application) {
         application.routing {
             route("/users") {
-                get {
-                    val users = userRepository.getAll()
-                        .map { userAggregate -> User(userAggregate) }
-
-                    call.respond(users)
-                }
-
-                post {
-                    TODO("create user")
-                }
+                get { list(call) }
+                post { create(call) }
 
                 route("{id}") {
-                    get {
-                        val rawId = call.parameters["id"] ?: return@get call.respond(HttpStatusCode.BadRequest)
-
-                        val id = try {
-                            Id(rawId)
-                        } catch (e: Throwable) {
-                            return@get call.respond(HttpStatusCode.BadRequest)
-                        }
-
-                        val userAggregate = userRepository.getById(id)
-                        if (userAggregate == null) {
-                            call.respond(HttpStatusCode.NotFound)
-                            return@get
-                        }
-
-                        val userModel = User(userAggregate)
-                        call.respond(userModel)
-                    }
-
-                    put {
-                        TODO("update user")
-                    }
-
-                    delete {
-                        val rawId = call.parameters["id"] ?: return@delete call.respond(HttpStatusCode.BadRequest)
-
-                        val id = try {
-                            Id(rawId)
-                        } catch (e: Throwable) {
-                            return@delete call.respond(HttpStatusCode.BadRequest)
-                        }
-
-                        val userAggregate = userRepository.getById(id)
-                        if (userAggregate == null) {
-                            call.respond(HttpStatusCode.NotFound)
-                            return@delete
-                        }
-
-                        userRepository.delete(userAggregate)
-
-                        call.respond(HttpStatusCode.OK)
-                    }
+                    get { detail(call) }
+                    put { update(call) }
+                    delete { delete(call) }
                 }
             }
         }
+    }
+
+    suspend fun list(call: ApplicationCall) {
+        val users = userRepository.getAll()
+            .map { userAggregate -> User(userAggregate) }
+
+        call.respond(users)
+    }
+
+    suspend fun detail(call: ApplicationCall) {
+        val rawId = call.parameters["id"] ?: return call.respond(HttpStatusCode.BadRequest)
+
+        val id = try {
+            Id(rawId)
+        } catch (e: Throwable) {
+            return call.respond(HttpStatusCode.BadRequest)
+        }
+
+        val userAggregate = userRepository.getById(id)
+        if (userAggregate == null) {
+            call.respond(HttpStatusCode.NotFound)
+            return
+        }
+
+        val userModel = User(userAggregate)
+        call.respond(userModel)
+    }
+
+    suspend fun create(call: ApplicationCall) {
+        TODO("create user")
+    }
+
+    suspend fun update(call: ApplicationCall) {
+        TODO("update user")
+    }
+
+    suspend fun delete(call: ApplicationCall) {
+        val rawId = call.parameters["id"] ?: return call.respond(HttpStatusCode.BadRequest)
+
+        val id = try {
+            Id(rawId)
+        } catch (e: Throwable) {
+            return call.respond(HttpStatusCode.BadRequest)
+        }
+
+        val userAggregate = userRepository.getById(id)
+        if (userAggregate == null) {
+            call.respond(HttpStatusCode.NotFound)
+            return
+        }
+
+        userRepository.delete(userAggregate)
+
+        call.respond(HttpStatusCode.OK)
     }
 }
