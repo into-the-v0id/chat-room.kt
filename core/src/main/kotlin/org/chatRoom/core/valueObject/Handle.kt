a02895596582dec1f436b8c679008e9c82c5108f
Void <57257404+into-the-v0id@users.noreply.github.com>
2022-11-02 11:27:21 +0100

    Remove unused code

--- core/src/main/kotlin/org/chatRoom/core/valueObject/Handle.kt
+++ core/src/main/kotlin/org/chatRoom/core/valueObject/Handle.kt
@@ -5,16 +5,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 @JvmInline
 value class Handle(private val handle: String) {
-    companion object {
-        fun tryFrom(id: String): Handle? {
-            return try {
-                Handle(id)
-            } catch (e: Throwable) {
-                null
-            }
-        }
-    }
-
     init {
         if (! handle.matches(Regex("^[a-zA-Z0-9\\-_]+\$"))) error("Invalid handle")
         if (handle.length > 50) error("Invalid handle")
--- core/src/main/kotlin/org/chatRoom/core/valueObject/Id.kt
+++ core/src/main/kotlin/org/chatRoom/core/valueObject/Id.kt
@@ -6,16 +6,6 @@ import java.util.UUID
 @Serializable
 @JvmInline
 value class Id(private val id: String) {
-    companion object {
-        fun tryFrom(id: String): Id? {
-            return try {
-                Id(id)
-            } catch (e: Throwable) {
-                null
-            }
-        }
-    }
-
     constructor(): this(UUID.randomUUID().toString())
 
     init {
