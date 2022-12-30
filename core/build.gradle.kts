4666a2b004bdb8f385d66ff8168d476cb09e98cf
Void <57257404+into-the-v0id@users.noreply.github.com>
2022-12-30 12:23:43 +0100

    Use UUIDv7 instead of UUIDv4

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation("org.slf4j:slf4j-api:1.7.36")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
     implementation("commons-validator:commons-validator:1.7")
+    implementation("com.github.f4b6a3:uuid-creator:5.2.0")
 
     testImplementation(kotlin("test"))
 }
--- core/src/main/kotlin/org/chatRoom/core/valueObject/Id.kt
+++ core/src/main/kotlin/org/chatRoom/core/valueObject/Id.kt
@@ -1,16 +1,17 @@
 package org.chatRoom.core.valueObject
 
+import com.github.f4b6a3.uuid.UuidCreator
+import com.github.f4b6a3.uuid.util.UuidValidator
 import kotlinx.serialization.Serializable
 import java.util.UUID
 
 @Serializable
 @JvmInline
 value class Id(private val id: String) {
-    constructor(): this(UUID.randomUUID().toString())
+    constructor(): this(UuidCreator.getTimeOrderedEpoch().toString())
 
     init {
-        // validate UUID
-        UUID.fromString(id)
+        require(UuidValidator.isValid(id)) { "Invalid UUID" }
     }
 
     override fun toString(): String = id
