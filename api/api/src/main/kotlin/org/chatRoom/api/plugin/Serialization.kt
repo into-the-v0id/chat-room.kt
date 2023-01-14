e3b2abaea43a619cafaa07913d78558ed4b8c375
Void <57257404+into-the-v0id@users.noreply.github.com>
2023-01-14 23:34:46 +0100

    [api] Enable CBOR serializer module. Pretty print JSON.

--- api/api/src/main/kotlin/org/chatRoom/api/plugin/Serialization.kt
+++ api/api/src/main/kotlin/org/chatRoom/api/plugin/Serialization.kt
@@ -1,11 +1,22 @@
 package org.chatRoom.api.plugin
 
+import io.ktor.serialization.kotlinx.cbor.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.application.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.cbor.Cbor
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
+        json(Json {
+            encodeDefaults = true
+            prettyPrint = true
+        })
+        @OptIn(ExperimentalSerializationApi::class)
+        cbor(Cbor {
+            encodeDefaults = true
+        })
     }
 }
