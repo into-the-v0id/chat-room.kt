a53ae47ceb561e27f21e4ae6b075e1ab20b130a4
Void <57257404+into-the-v0id@users.noreply.github.com>
2023-02-01 17:39:01 +0100

    [api] Fixed repository call

--- api/api/src/main/kotlin/org/chatRoom/api/repository/write/guard/MessageWriteGuardRepository.kt
+++ api/api/src/main/kotlin/org/chatRoom/api/repository/write/guard/MessageWriteGuardRepository.kt
@@ -2,6 +2,7 @@ package org.chatRoom.api.repository.write.guard
 
 import org.chatRoom.core.aggreagte.Message
 import org.chatRoom.core.repository.Transaction
+import org.chatRoom.core.repository.read.MessageQuery
 import org.chatRoom.core.repository.read.MessageReadRepository
 import org.chatRoom.core.repository.write.MessageWriteRepository
 
@@ -11,14 +12,14 @@ class MessageWriteGuardRepository(
 ) : MessageWriteRepository {
     override suspend fun createAll(messages: Collection<Message>, transaction: Transaction) {
         val messageIds = messages.map { message -> message.modelId }
-        if (messageReadRepository.getAll(ids = messageIds).isNotEmpty()) error("Unable to create all specified messages: Message already exists")
+        if (messageReadRepository.getAll(MessageQuery(ids = messageIds)).isNotEmpty()) error("Unable to create all specified messages: Message already exists")
 
         repository.createAll(messages, transaction)
     }
 
     override suspend fun updateAll(messages: Collection<Message>, transaction: Transaction) {
         val messageIds = messages.map { message -> message.modelId }
-        val allIdsExist = messageReadRepository.getAll(ids = messageIds)
+        val allIdsExist = messageReadRepository.getAll(MessageQuery(ids = messageIds))
             .map { message -> message.modelId }
             .containsAll(messageIds)
         if (! allIdsExist) error("Unable to update all specified messages: Message not found")
@@ -28,7 +29,7 @@ class MessageWriteGuardRepository(
 
     override suspend fun deleteAll(messages: Collection<Message>, transaction: Transaction) {
         val messageIds = messages.map { message -> message.modelId }
-        val allIdsExist = messageReadRepository.getAll(ids = messageIds)
+        val allIdsExist = messageReadRepository.getAll(MessageQuery(ids = messageIds))
             .map { message -> message.modelId }
             .containsAll(messageIds)
         if (! allIdsExist) error("Unable to delete all specified messages: Message not found")
